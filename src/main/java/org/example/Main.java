6a00eb309fe15398769058410a9686bf731f5d30
rashmonidey <rashudey9851@gmail.com>
2023-02-17 13:25:22 +0100

    Using binary search

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,20 @@
 package org.example;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
-
-
-        Scanner scanner;
-        try {
-            scanner = new Scanner(new File("sample_prefixes.txt"));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        ArrayList<String> prefixList = new ArrayList<>();
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("sample_prefixes.txt"));
+        ArrayList<String> prefixList = new ArrayList<String>();
         while (scanner.hasNext()){
             prefixList.add(scanner.next());
         }
         scanner.close();
 
-        //Input string
-        String inputString = "Rhp1IhPabcd";
+        String inputString = "HYsHo4jasdjahsdkj";
 
         String longestPrefix = findLongestPrefix(inputString, prefixList);
 
@@ -37,16 +27,16 @@ public class Main {
     public static String findLongestPrefix(String inputString, List<String> prefixList) {
         String longestPrefix = "";
 
-        String startingLetter = inputString.substring(0,1);
+        Collections.sort(prefixList);
 
-        List<String> filteredList = prefixList.stream().filter(s->s.startsWith(startingLetter)).toList();
-
-
-        for (int i = 1;i<filteredList.size();i++) {
-            if (inputString.startsWith(filteredList.get(i)) && filteredList.get(i).length() > longestPrefix.length()){
-                longestPrefix = filteredList.get(i);
+        for (int i = 0;i<inputString.length();i++){
+            String searchKey = inputString.substring(0,i);
+            if(Collections.binarySearch(prefixList,searchKey)>0){
+                int index = Collections.binarySearch(prefixList,searchKey);
+                longestPrefix = prefixList.get(index);
             }
         }
+
         return longestPrefix;
     }
 }
